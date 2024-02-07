6724165bbca10b5cc45e151eaa510970b3da1039
William Martins <williamdabruzzmartins76136@gmail.com>
2024-02-07 13:56:54 -0300

    test

--- cmd/QuizMeDeutsch/main.go
+++ cmd/QuizMeDeutsch/main.go
@@ -1,23 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"github.com/WilliamKotoco/QuizMeDeutsch/internal/datenbetreiber"
-)
+import "github.com/WilliamKotoco/QuizMeDeutsch/internal/gui"
 
 func main() {
+	gui.Test()
 
-	var test datenbetreiber.Datenbetreiber
-
-	test = &datenbetreiber.ZufälligDatenbetreiber{
-		Frage:   "",
-		Antwort: "",
-	}
-
-	test.LadenFrageAntwort()
-
-	zufälligDatenbetreiber := test.(*datenbetreiber.ZufälligDatenbetreiber)
-
-	fmt.Println(zufälligDatenbetreiber.Frage)
-	fmt.Println(zufälligDatenbetreiber.Antwort)
 }
--- internal/gui/hauptfenster.go
+++ internal/gui/hauptfenster.go
@@ -1,1 +1,15 @@
 package gui
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func Test() {
+	a := app.New()
+	w := a.NewWindow("Hello world")
+
+	w.SetContent(widget.NewLabel("Hello world"))
+	w.ShowAndRun()
+
+}
